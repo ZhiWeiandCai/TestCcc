e5de9534c4dc93d926eac27818e3c1bef6a24acc
czw <543481852@qq.com>
2025-05-18 14:00:29 +0800

    init proj

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * created by czw on ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
